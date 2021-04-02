e821946ec689af6ce64e6e023d2ceb06f6ab9040
max-student <max-student@maxtrain.com>
2021-04-02 15:45:04 -0400

    commit users

---
+++ src/app/menu/menu.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css']
+})
+export class MenuComponent implements OnInit {
+
+  menus: any[] = [    //create menu property
+    {display:'Home', route: '/home'},
+    {display:'Users', route: '/users/list'},
+    {display:'About', route: '/about'},
+    {display:'Help', route: '/help'},
+    {display:'Login', route:'/login'}
+    
+  ]
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/user/user-list/user-list.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from '../user.class';
+import { UserService } from '../user.service';
+
+@Component({
+  selector: 'app-user-list',
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.css']
+})
+export class UserListComponent implements OnInit {
+
+  users: User[] = [];// create property (empty array)
+  searchCriteria: string = "";
+
+  constructor(
+    private usrsrv: UserService  //Inject User service
+  ) { }
+
+  ngOnInit(): void {
+    this.usrsrv.list().subscribe(
+      users => {
+        console.log("Users:", users)
+        this.users = users;
+
+      },
+      err => {
+        console.error(err);
+      }
+
+    );
+  }
+
+}
+
+
