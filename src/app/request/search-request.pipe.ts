210541dd1d229c072d0cffa83bf288d20805bfe9
max-student <max-student@maxtrain.com>
2021-04-05 20:54:53 -0400

    updated

---
+++ src/app/request/search-request.pipe.ts
@@ -0,0 +1,24 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'searchRequest'
+})
+export class SearchRequestPipe implements PipeTransform {
+
+  transform(value: unknown, ...args: unknown[]): unknown {
+    return null;
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+  
+}
